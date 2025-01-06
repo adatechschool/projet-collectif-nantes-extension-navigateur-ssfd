996385a65537c8c9158e86e421764dfe36a40123
Sebus83 <sebastien.herbiet@gmail.com>
2025-01-06 16:34:51 +0100

    border red

--- greenh1.js
+++ greenh1.js
@@ -1,21 +1,34 @@
-document.addEventListener("DOMContentLoaded",() =>{
-    document.getElementById("green-h1").addEventListener("click", ()=>{
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>{
-            chrome.scripting.executeScript({
-                target:{tabId: tabs[0].id},
-                func : greenH1
-            })
-        }
-
-        )
-    }
-)
-}
-)
- function greenH1(){
-    console.log("hello")
-    const h1s = document.querySelectorAll('h1');
-    h1s.forEach(h1 =>{
-        h1.style.color ='green';
-    })
- }
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("green-h1").addEventListener("click", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        // Premier appel pour greenH1
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          func: greenH1
+        });
+        
+        // Deuxième appel pour redBorder
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          func: redBorder
+        });
+      });
+    });
+  });
+  
+  function greenH1() {
+    console.log("hello");
+    const h1s = document.querySelectorAll("h1");
+    h1s.forEach((h1) => {
+      h1.style.color = "green";
+    });
+  }
+  
+  function redBorder() {
+    console.log("hello 2");
+    const images = document.querySelectorAll("img");
+    images.forEach((image) => {
+      image.style.border = "solid 4px red";
+    });
+  }
+  
