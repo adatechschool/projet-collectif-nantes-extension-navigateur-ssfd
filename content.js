2047d2459200c5e782dfa9240449764ab775bb57
baehl <baehl44@yahoo.fr>
2025-01-07 12:28:23 +0100

    extension choisir une image

--- content.js
+++
@@ -1,6 +0,0 @@
-const images = document.querySelectorAll('img')
-
-images.forEach ((image) => {
-    image.src = chrome.runtime.getURL("photo/grumpyCat.png")
-    image.alt = "photo de Grumpy Cat"
-})
