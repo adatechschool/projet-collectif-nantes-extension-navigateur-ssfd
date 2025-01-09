c523bb5709b41624982e4ba6dbaeac7431ea01d1
baehl <baehl44@yahoo.fr>
2025-01-09 10:09:58 +0100

    nouvelle branche sarra-dev

--- background.js
+++ background.js
@@ -1,28 +1,71 @@
-let time = 2;
-let intervalId = null;
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "greeting-30") {
+        console.log("Message reçu depuis popup.js :", message.data);
+        // Créer une alarme pour 30 minutes
+        chrome.alarms.create('pauseReminder-30', {
+            delayInMinutes: 0.1
+        });
+        sendResponse({ response: "La pause est dans 30 minutes" });
+    } else if (message.type === "greeting-60") {
+        console.log("Message reçu depuis popup.js :", message.data);
+        // Créer une alarme pour 60 minutes
+        chrome.alarms.create('pauseReminder-60', {
+            delayInMinutes: 60
+        });
+        sendResponse({ response: "La pause est dans 60 minutes" });
+    }
+});
+
+// Gestionnaire d'événements pour les alarmes:creation d'une notification:
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'pauseReminder-30') {
+        chrome.notifications.create('pauseNotification-30', {
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Vous mértiez une petite pause ZEN 💆🏻',
+            message: 'Il est temps de faire une pause de 5 minutes',
+            priority: 2,
+            requireInteraction: true,
+            buttons: [
+                { title: 'OK, je fais une pause' },
+                { title: 'Reporter de 5 minutes' }
+            ]
+        });
+    } else if (alarm.name === 'pauseReminder-60') {
+        chrome.notifications.create('pauseNotification-60', {
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Vous mértiez une petite pause ZEN 💆🏻',
+            message: 'Il est temps de faire une pause de 10 minutes',
+            priority: 2,
+            requireInteraction: true,
+            buttons: [
+                { title: 'OK, je fais une pause' },
+                { title: 'Reporter de 5 minutes' }
+            ]
+        });
+    }
+});
+// Gestionnaire pour les boutons des notifications
+chrome.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
+    if (notificationId === 'pauseNotification-30' || notificationId === 'pauseNotification-60') {
+        if (buttonIndex === 0) {
+            console.log("L'utilisateur commence une pause !");
+        } else if (buttonIndex === 1) {
+            console.log("L'utilisateur reporte la pause de 5 minutes.");
+            // Reprogrammer une alarme pour 5 minutes supplémentaires
+            chrome.alarms.create('pauseReminder-5', {
+                delayInMinutes: 0.1
+            });
+        }
+    }
+});
 
 //alimentation du stockage locale
-chrome.storage.local.set({ key: "Delphine" }).then(() => {
+chrome.storage.local.set({ key: "Welcome!" }).then(() => {
     console.log("Value is set");
   });
   
 
 chrome.action.openPopup()
- 
-function startTimer() {
-    time = 2;
-    intervalId = setInterval(updateTimer, 1000);
-}
-
-function updateTimer() {
-    time--;
-    if (time < 0) {
-        time = 2;
-    }
-}
-
-function stopTimer() {
-    clearInterval(intervalId);
-}
-
-startTimer();
--- popup.js
+++ popup.js
@@ -1,36 +1,21 @@
-let timer = document.getElementById("timer");
-// let time = 2;
-const MESSAGE = document.getElementById("zen-message");
-// let intervalId = null;
-
-// function startTimer() {
-//     time = 2;
-//     MESSAGE.innerHTML = "";
-//     intervalId = setInterval(updateTimer, 1000);
-// }
-
-// function updateTimer() {
-//     timer.innerHTML = "00:" + "0"+time;
-//     time--;
-    // if (time < 0) {
-    //     displayZenMessage();
-        
-    //     setTimeout(() => {
-    //         time = 2;
-    //         MESSAGE.innerHTML = ""; 
-    //     }, 1000);
-    // }
-// }
-
-// function stopTimer() {
-//     clearInterval(intervalId);
-// }
-
-function displayZenMessage() {    
-    MESSAGE.innerHTML = "très doux, très gentil, avec des coeurs ! :)";
-}
-displayZenMessage()
-// startTimer();
+document.getElementById("time-30min").addEventListener("click", () => {
+  chrome.runtime.sendMessage(
+    { type: "greeting-30", data: "Bonjour depuis popup.js !" },
+    (response) => {
+      console.log("Réponse du background.js :", response.response);
+      document.getElementById("response").innerText = response.response;
+    }
+  );
+});
+document.getElementById("time-60min").addEventListener("click", () => {
+  chrome.runtime.sendMessage(
+    { type: "greeting-60", data: "Bonjour depuis popup.js !" },
+    (response) => {
+      console.log("Réponse du background.js :", response.response);
+      document.getElementById("response").innerText = response.response;
+    }
+  );
+});
 chrome.storage.local.get(["key"]).then((result) => {
-    alert("Value is " + result.key);
-  });
+  alert(result.key);
+});
