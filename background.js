6f307d1e8114c3e3dbb281e13bee03f6dcab1d0f
baehl <baehl44@yahoo.fr>
2025-01-13 20:39:52 +0100

    resolution conflits seb sarra florence

--- background.js
+++ background.js
@@ -48,29 +48,6 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 });
 //fonction pour généner une vidéo aléatoire:
 const urls = [
-    "https://www.youtube.com/watch?v=jCjpY0-7Rs0",
-    "https://www.youtube.com/watch?v=mIeUmvy2b-k",
-    "https://www.youtube.com/watch?v=ssss7V1_eyA",
-    "https://www.youtube.com/watch?v=inpok4MKVLM",
-    "https://www.youtube.com/watch?v=f3N2QrQMCsQ",
-    "https://www.youtube.com/watch?v=FkcPe_BsX70&list=PL8qGgKC5WkTQsa-dTqRY6-EMJkdUNxieN&index=3",
-    "https://www.youtube.com/watch?v=fZCUYYW3W8c&list=PL8qGgKC5WkTQsa-dTqRY6-EMJkdUNxieN&index=11"
-    
-  ];
-function getRandomUrl(){
-    return urls[Math.floor(Math.random() * urls.length)];
-}
-// fonction qui crée un nouvel onglet avec une vidéo aléatoire
-function watchZenVideo() {
-  chrome.tabs.create({
-    url: getRandomUrl(),
-    active: true, // L'onglet sera actif (au premier plan)
-  });
-}
-
-
-//fonction pour généner une vidéo aléatoire:
-const urls = [
   "https://www.youtube.com/watch?v=jCjpY0-7Rs0",
   "https://www.youtube.com/watch?v=mIeUmvy2b-k",
   "https://www.youtube.com/watch?v=ssss7V1_eyA",
